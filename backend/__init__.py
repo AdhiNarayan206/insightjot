c4f96be16d03c04c6d668774c1fae1f9a955f2e9
AdhiNarayan206 <adhinarayan206@gmail.com>
2025-08-13 22:32:36 +0530

    added login and sign up form in the backend

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from backend import pages
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'
     CORS(app)
     app.register_blueprint(pages.bp)
     return app
